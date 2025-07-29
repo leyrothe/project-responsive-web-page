72cef2ff2e4315dca4533a301867bf6d3802fee8
Petra Husárová <>
2025-07-29 10:57:17 +0200

    code cleanup

--- script.js
+++ script.js
@@ -5,32 +5,25 @@ const navContainer = document.querySelector('.nav-container');
 let navigationOpen = false;
 
 hamburger.addEventListener('click',() => {
-    /*toggle bool everytime hamburger icon is clicked*/
+    // toggle bool everytime hamburger icon is clicked
     navigationOpen = !navigationOpen;
-    /*if navigation is open than add classes = show menu*/
-    if(navigationOpen){
+
+    // if navigation is open then add classes = show menu
+    if (navigationOpen) {
      navContainer.classList.add('navigation-mobile');
      navList.classList.add('nav-list-mobile');
-    
-    }else{
-    /*else if navigation is closed remove classes = hide menu*/
+    } else {
+    // else if navigation is closed remove classes = hide menu
      navContainer.classList.remove('navigation-mobile');
      navList.classList.remove('nav-list-mobile');
- 
     }
-    
 });
 
 window.addEventListener('resize', () => {
   if (window.innerWidth >= 992) {
-    // Desktop view
+    // remove mobile nav classes for desktop view
     navContainer.classList.remove('navigation-mobile');
     navList.classList.remove('nav-list-mobile');
-    navigationOpen = false;
-  } else {
-    if (!navigationOpen) {
-      navigationOpen = false;
-    }
-  }
+  } 
+  navigationOpen = false;
 });
-
